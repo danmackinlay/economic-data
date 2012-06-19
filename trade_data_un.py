c317dfd6ba4b12d63fff77ba1f3cd52ddb8eefd7
howthebodyworks <git@email.possumpalace.org>
2012-06-19 19:56:52 +1000

    un trade search data annotated

---
+++ trade_data_un.py
@@ -0,0 +1,6 @@
+"""
+SITC rev 4 trade codes are documented at URLs like this:
+http://unstats.un.org/unsd/cr/registry/regcs.asp?Cl=14&Lg=1&Co=723.21
+A complete list of codes is in S4BH.xls, and an unparseable plaintext in SeriesM_34rev4E.pdf but the UN web pages will need to be scraped for the whole goss.
+http://unstats.un.org/unsd/cr/registry/regcs.asp?Cl=28&Lg=1&Co=121
+"""
