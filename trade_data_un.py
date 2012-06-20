a28e34bed5f4e3cd4290d96cbc3cb75613e1dd65
howthebodyworks <git@email.possumpalace.org>
2012-06-20 14:54:57 +1000

    steps toward importing data to MySQL from R

--- trade_data_un.py
+++ trade_data_un.py
@@ -4,3 +4,13 @@ http://unstats.un.org/unsd/cr/registry/regcs.asp?Cl=14&Lg=1&Co=723.21
 A complete list of codes is in S4BH.xls, and an unparseable plaintext in SeriesM_34rev4E.pdf but the UN web pages will need to be scraped for the whole goss.
 http://unstats.un.org/unsd/cr/registry/regcs.asp?Cl=28&Lg=1&Co=121
 """
+
+#NB rpy2 only compiles for 64 bit python out of the box.
+from rpy2.robjects.packages import importr
+
+#this works, but is astonishingly slow. Too slow to be useable.
+foreign = importr('foreign')
+wtf=foreign.read_dta("/Users/dan/Desktop/researchtmp/un/wtf62.dta")
+col_names = wtf.names
+for row in wtf.iter_row():
+    print dict(zip(col_names, [e[0] for e in row]))
