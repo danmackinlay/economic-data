fe6226f8d3dd6e454ca6c60d91648a56e59d3ba9
howthebodyworks <git@email.possumpalace.org>
2012-07-12 10:44:22 +1000

    wrap the messy state vars in a function

--- equity_data.R
+++ equity_data.R
@@ -3,29 +3,28 @@
 library("lmtest")
 
 base.path = '/Users/dan/Dropbox/trade_data/cache/'
-files = list.files(base.path, pattern=".*\\.csv\\.gz")
-favourite.equities.ticker.names = c('AAI', 'AAT', 'AAU', 'AAY', 'AEF', 'AGK', 'AIZ', 'ALL', 'AMP', 'ANN', 'ANZ', 'APD', 'APN', 'APP', 'AQF', 'ARG', 'ASX', 'AVH', 'BBG', 'BEN', 'BGA', 'BHP', 'BKL', 'BOQ', 'BXB', 'CBA', 'CCL', 'CCV', 'CER', 'CLO', 'CLX', 'CMJ', 'CNG', 'CRF', 'CSR', 'CTY', 'CWN', 'CYU', 'DJS', 'DMP', 'EAU', 'ELD', 'FLT', 'FMG', 'FPA', 'FPH', 'FXJ', 'GDA', 'GEM', 'GFF', 'GMG', 'HRL', 'HVN', 'IFL', 'IFM', 'ION', 'JBH', 'JET', 'KMD', 'LCT', 'LMW', 'MGI', 'MGM', 'MGR', 'MIX', 'MQG', 'MUE', 'MXU', 'MYR', 'NAB', 'NCM', 'NVT', 'NWS', 'OEC', 'OMI', 'ORD', 'ORG', 'ORI', 'OST', 'PBG', 'PHG', 'PNW', 'QAN', 'QBE', 'QRN', 'RRS', 'RUM', 'SBK', 'SGH', 'SGP', 'SGT', 'SIP', 'SKC', 'SKT', 'SOL', 'SUN', 'SWM', 'SXL', 'SYD', 'TAH', 'TEL', 'TEN', 'TIS', 'TLS', 'TPC', 'TRS', 'TSE', 'TTS', 'VEI', 'VEL', 'VRL', 'VSC', 'VTG', 'WBC', 'WDC', 'WEB', 'WES', 'WFA', 'WFT', 'WOW', 'WRT', 'WSF', 'WTF', 'WWM', 'ZBI', 'ZRI')
-i=0
-limit=10
 
-equities = data.frame()
-equity.tickers = list()
+get.favourite.indices = function (limit=10) {
+  
+  files = list.files(base.path, pattern=".*\\.csv\\.gz")
+  favourite.equities.ticker.names = c('AAI', 'AAT', 'AAU', 'AAY', 'AEF', 'AGK', 'AIZ', 'ALL', 'AMP', 'ANN', 'ANZ', 'APD', 'APN', 'APP', 'AQF', 'ARG', 'ASX', 'AVH', 'BBG', 'BEN', 'BGA', 'BHP', 'BKL', 'BOQ', 'BXB', 'CBA', 'CCL', 'CCV', 'CER', 'CLO', 'CLX', 'CMJ', 'CNG', 'CRF', 'CSR', 'CTY', 'CWN', 'CYU', 'DJS', 'DMP', 'EAU', 'ELD', 'FLT', 'FMG', 'FPA', 'FPH', 'FXJ', 'GDA', 'GEM', 'GFF', 'GMG', 'HRL', 'HVN', 'IFL', 'IFM', 'ION', 'JBH', 'JET', 'KMD', 'LCT', 'LMW', 'MGI', 'MGM', 'MGR', 'MIX', 'MQG', 'MUE', 'MXU', 'MYR', 'NAB', 'NCM', 'NVT', 'NWS', 'OEC', 'OMI', 'ORD', 'ORG', 'ORI', 'OST', 'PBG', 'PHG', 'PNW', 'QAN', 'QBE', 'QRN', 'RRS', 'RUM', 'SBK', 'SGH', 'SGP', 'SGT', 'SIP', 'SKC', 'SKT', 'SOL', 'SUN', 'SWM', 'SXL', 'SYD', 'TAH', 'TEL', 'TEN', 'TIS', 'TLS', 'TPC', 'TRS', 'TSE', 'TTS', 'VEI', 'VEL', 'VRL', 'VSC', 'VTG', 'WBC', 'WDC', 'WEB', 'WES', 'WFA', 'WFT', 'WOW', 'WRT', 'WSF', 'WTF', 'WWM', 'ZBI', 'ZRI')
+  i=0
 
-for (file.name in files) {
-  ticker.name = substr(file.name, 1, 3)
+  equities = data.frame()
+  equity.tickers = list()
+
+  for (file.name in files) {
+    ticker.name = substr(file.name, 1, 3)
   
-  if(!(ticker.name %in% favourite.equities.ticker.names)) {next}
-  if(i>limit) {break}
-  print(ticker.name)
-  append(equity.tickers, ticker.name)
+    if(!(ticker.name %in% favourite.equities.ticker.names)) {next}
+    if(i>limit) {break}
+    print(ticker.name)
+    append(equity.tickers, ticker.name)
   
-  one.equity = read.csv(gzfile(paste(base.path, file.name, sep = "")))
-  trimmed.equity = one.equity[c("Date")]
-  trimmed.equity[ticker.name] = log(one.equity["Adj.Close"])
-  equities = merge(equities, trimmed.equity, all.x=TRUE, all.y=TRUE)
-  
-  #can i avoid needing this by using a closure?
-  #R syntax. bah.
-  rm(one.equity)
-  rm(trimmed.equity)
+    one.equity = read.csv(gzfile(paste(base.path, file.name, sep = "")))
+    trimmed.equity = one.equity[c("Date")]
+    trimmed.equity[ticker.name] = log(one.equity["Adj.Close"])
+    equities = merge(equities, trimmed.equity, all.x=TRUE, all.y=TRUE)
+  }
+  return(equities)
 }
