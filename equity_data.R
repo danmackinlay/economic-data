59533106a6d51ae01a77493aab2d6a98c38b53ac
howthebodyworks <git@email.possumpalace.org>
2012-07-23 15:38:13 +1000

    break out path specs to allow an SQLite DB to fit in there

--- equity_data.R
+++ equity_data.R
@@ -9,12 +9,12 @@ library("ggdendro")
 library("RSQLite")
 
 
-
-base.path = '/Users/dan/Dropbox/trade_data/cache/'
+base.path = '/Users/dan/Dropbox/trade_data'
+cache.path = paste(base.path, "cache", sep="/")
 
 get.favourite.indices = function (limit=10) {
-  
-  files = list.files(base.path, pattern=".*\\.csv\\.gz")
+
+  files = list.files(cache.path, pattern=".*\\.csv\\.gz")
   favourite.equities.ticker.names = c('AAI', 'AAT', 'AAU', 'AAY', 'AEF', 'AGK', 'AIZ', 'ALL', 'AMP', 'ANN', 'ANZ', 'APD', 'APN', 'APP', 'AQF', 'ARG', 'ASX', 'AVH', 'BBG', 'BEN', 'BGA', 'BHP', 'BKL', 'BOQ', 'BXB', 'CBA', 'CCL', 'CCV', 'CER', 'CLO', 'CLX', 'CMJ', 'CNG', 'CRF', 'CSR', 'CTY', 'CWN', 'CYU', 'DJS', 'DMP', 'EAU', 'ELD', 'FLT', 'FMG', 'FPA', 'FPH', 'FXJ', 'GDA', 'GEM', 'GFF', 'GMG', 'HRL', 'HVN', 'IFL', 'IFM', 'ION', 'JBH', 'JET', 'KMD', 'LCT', 'LMW', 'MGI', 'MGM', 'MGR', 'MIX', 'MQG', 'MUE', 'MXU', 'MYR', 'NAB', 'NCM', 'NVT', 'NWS', 'OEC', 'OMI', 'ORD', 'ORG', 'ORI', 'OST', 'PBG', 'PHG', 'PNW', 'QAN', 'QBE', 'QRN', 'RRS', 'RUM', 'SBK', 'SGH', 'SGP', 'SGT', 'SIP', 'SKC', 'SKT', 'SOL', 'SUN', 'SWM', 'SXL', 'SYD', 'TAH', 'TEL', 'TEN', 'TIS', 'TLS', 'TPC', 'TRS', 'TSE', 'TTS', 'VEI', 'VEL', 'VRL', 'VSC', 'VTG', 'WBC', 'WDC', 'WEB', 'WES', 'WFA', 'WFT', 'WOW', 'WRT', 'WSF', 'WTF', 'WWM', 'ZBI', 'ZRI')
   i=0
 
@@ -28,9 +28,9 @@ get.favourite.indices = function (limit=10) {
     
     i=i+1
       
-    one.equity = read.csv(gzfile(paste(base.path, file.name, sep = "")))
+    one.equity = read.csv(gzfile(paste(cache.path, file.name, sep = "/")))
     trimmed.equity = one.equity[c("Date")]
-    #take max because some share trade at 0 (granularity is $0.01, zero is -4.61)
+    #take max because some shares still trade at 0 (granularity is $0.01, zero is -4.61)
     trimmed.equity[ticker.name] = log(pmax(
       one.equity["Adj.Close"],
       0.005))
