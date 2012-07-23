bf782dd231e3ecdb040bb529caa9eb7c091775d5
howthebodyworks <git@email.possumpalace.org>
2012-07-23 13:35:53 +1000

    let local settings override globals

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 import os.path
-try:
-	import _local_settings
-except ImportError:
-	pass
 
 EQUITY_CACHE_DIR = os.path.expanduser('~/Dropbox/trade_data/cache')
+
+try:
+	from _local_settings import *
+except ImportError:
+	pass
