be1d2ccf752f5c18947566b7c8149aa77dad236a
howthebodyworks <git@email.possumpalace.org>
2012-07-25 11:20:10 +1000

    make favourite.equities a clobally accessible vector

--- equity_data.R
+++ equity_data.R
@@ -9,9 +9,23 @@ library("reshape2")
 library("RSQLite")
 
 # example usage:
-# equities = pairwise.granger.test(get.favourite.indices(limit=Inf))
-# #equities.m = pairwise.granger.test.m(get.favourite.indices(limit=Inf))
+# equities = pairwise.granger.test(get.favourite.equities(limit=Inf))
+# #equities.m = pairwise.granger.test.m(get.favourite.equities(limit=Inf))
 
+favourite.equities = c('AAI', 'AAT', 'AAU', 'AAY',
+  'AEF', 'AGK', 'AIZ', 'ALL', 'AMP', 'ANN', 'ANZ', 'APD', 'APN', 'APP',
+  'AQF', 'ARG', 'ASX', 'AVH', 'BBG', 'BEN', 'BGA', 'BHP', 'BKL', 'BOQ',
+  'BXB', 'CBA', 'CCL', 'CCV', 'CER', 'CLO', 'CLX', 'CMJ', 'CNG', 'CRF',
+  'CSR', 'CTY', 'CWN', 'CYU', 'DJS', 'DMP', 'EAU', 'ELD', 'FLT', 'FMG',
+  'FPA', 'FPH', 'FXJ', 'GDA', 'GEM', 'GFF', 'GMG', 'HRL', 'HVN', 'IFL',
+  'IFM', 'ION', 'JBH', 'JET', 'KMD', 'LCT', 'LMW', 'MGI', 'MGM', 'MGR',
+  'MIX', 'MQG', 'MUE', 'MXU', 'MYR', 'NAB', 'NCM', 'NVT', 'NWS', 'OEC',
+  'OMI', 'ORD', 'ORG', 'ORI', 'OST', 'PBG', 'PHG', 'PNW', 'QAN', 'QBE',
+  'QRN', 'RRS', 'RUM', 'SBK', 'SGH', 'SGP', 'SGT', 'SIP', 'SKC', 'SKT',
+  'SOL', 'SUN', 'SWM', 'SXL', 'SYD', 'TAH', 'TEL', 'TEN', 'TIS', 'TLS',
+  'TPC', 'TRS', 'TSE', 'TTS', 'VEI', 'VEL', 'VRL', 'VSC', 'VTG', 'WBC',
+  'WDC', 'WEB', 'WES', 'WFA', 'WFT', 'WOW', 'WRT', 'WSF', 'WTF', 'WWM',
+  'ZBI', 'ZRI')
 
 base.path = '/Users/dan/Dropbox/trade_data'
 cache.path = paste(base.path, "cache", sep="/")
@@ -48,20 +62,7 @@ get.equities = function (limit=Inf, limit.to=NA) {
   return(equities)
 }
 get.favourite.equities = function (limit=Inf) {
-  return(get.equities(limit=limit, limit.to=c('AAI', 'AAT', 'AAU', 'AAY',
-    'AEF', 'AGK', 'AIZ', 'ALL', 'AMP', 'ANN', 'ANZ', 'APD', 'APN', 'APP',
-    'AQF', 'ARG', 'ASX', 'AVH', 'BBG', 'BEN', 'BGA', 'BHP', 'BKL', 'BOQ',
-    'BXB', 'CBA', 'CCL', 'CCV', 'CER', 'CLO', 'CLX', 'CMJ', 'CNG', 'CRF',
-    'CSR', 'CTY', 'CWN', 'CYU', 'DJS', 'DMP', 'EAU', 'ELD', 'FLT', 'FMG',
-    'FPA', 'FPH', 'FXJ', 'GDA', 'GEM', 'GFF', 'GMG', 'HRL', 'HVN', 'IFL',
-    'IFM', 'ION', 'JBH', 'JET', 'KMD', 'LCT', 'LMW', 'MGI', 'MGM', 'MGR',
-    'MIX', 'MQG', 'MUE', 'MXU', 'MYR', 'NAB', 'NCM', 'NVT', 'NWS', 'OEC',
-    'OMI', 'ORD', 'ORG', 'ORI', 'OST', 'PBG', 'PHG', 'PNW', 'QAN', 'QBE',
-    'QRN', 'RRS', 'RUM', 'SBK', 'SGH', 'SGP', 'SGT', 'SIP', 'SKC', 'SKT',
-    'SOL', 'SUN', 'SWM', 'SXL', 'SYD', 'TAH', 'TEL', 'TEN', 'TIS', 'TLS',
-    'TPC', 'TRS', 'TSE', 'TTS', 'VEI', 'VEL', 'VRL', 'VSC', 'VTG', 'WBC',
-    'WDC', 'WEB', 'WES', 'WFA', 'WFT', 'WOW', 'WRT', 'WSF', 'WTF', 'WWM',
-    'ZBI', 'ZRI')))
+  return(get.equities(limit=limit, limit.to=favourite.equities))
 }
 
 
